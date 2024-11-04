d0cd4ea10a06bcf10f23e1fb99df94a7b2744fcf
Ekaterina Balashova <balashova@road.ly>
2024-11-04 18:46:49 +0300

    debug wasm

--- src/worker/compile_wasm_no_simd_non_shared.sh
+++ src/worker/compile_wasm_no_simd_non_shared.sh
@@ -1,1 +1,1 @@
-em++ -std=c++11 sorter_no_simd.cpp -Os -s WASM=1 -s SIDE_MODULE=2 -o sorter_no_simd_non_shared.wasm -s IMPORTED_MEMORY=1
+em++ -std=c++11 -g sorter_no_simd.cpp -O0 -s WASM=1 -s SIDE_MODULE=2 -o sorter_no_simd_non_shared.wasm -s IMPORTED_MEMORY=1
