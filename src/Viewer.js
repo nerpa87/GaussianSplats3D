a18228a3892b8d5d520259c07a70c93c024cc6b2
Ekaterina Balashova <balashova@road.ly>
2025-02-14 13:58:55 +0300

    fix for JL rotation in case of complex orientation

--- src/Viewer.js
+++ src/Viewer.js
@@ -708,6 +708,10 @@ export class Viewer {
             }
 
             switch (code) {
+                case 'KeyN':
+                    let els = this.camera.matrix.elements;
+                    console.log('Current up directoion is', [els[4], els[5], els[6]]);
+                    break;
                 case 'KeyV':
                     this.showControlPlane = !this.showControlPlane;
                     break;
@@ -866,6 +870,10 @@ export class Viewer {
                     break;
                 case 'KeyL':
                 case 'KeyJ':
+                    // because of lookAt method inside controls.update we should be sure that normal is perpendicular to lookAt point
+                    let els = this.camera.matrix.elements;
+                    this.camera.up.set(els[4], els[5], els[6]);
+                    //
                     this.camera.rotateOnAxis(new Vector3(0,1,0), ((code == 'KeyL') ? -1 : 1) * rstep);
                     this.updateTargetFromCamera();
                     break;
